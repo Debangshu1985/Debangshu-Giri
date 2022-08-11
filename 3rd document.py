1978dfaa1cedb8b2c87a70bf2cdfff6ea26bcef9
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-11 20:30:45 +0530

    Add files via upload

---
+++ 3rd document.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+f=5
+g=8/9
+z=f-g
+print(z)
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[13]:
+
+
+pd.read_excel(r"C:\Users\user\Desktop\Complaint code Master - BSIV warranty policy.xlsx")
+
+
+# In[14]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\batch.csv")
+
+
+# In[15]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\Complaint code Master - BSIV warranty policy err.csv")
+
+
+# In[17]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\ttt.csv")
+
+
+# In[22]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\team.txt")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
