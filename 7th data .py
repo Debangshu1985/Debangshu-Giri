ba212c504d05cfa96b8cd14e8893df5819580857
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-14 08:37:03 +0530

    Add files via upload

---
+++ 7th data .py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+p=8
+d=6
+r=52
+e=r/(p+d)
+print(e)
+type(e)
+
+
+# In[5]:
+
+
+t=78
+f=26
+h=75
+t=15
+r=(t/f)+(h/t)
+print(r)
+type(r)
+
+
+# In[6]:
+
+
+t=78
+f=26
+r=(t/f)
+print(r)
+type(r)
+
+
+# In[8]:
+
+
+T=75
+R=15
+E=(T*R)
+print(E)
+type(E)
+
+
+# In[9]:
+
+
+import os
+
+
+# In[12]:
+
+
+os.getcwd()
+
+
+# In[23]:
+
+
+import pandas as pd
+
+
+# In[24]:
+
+
+pd.read_excel("data.xlsx")
+
+
+# In[25]:
+
+
+pd.read_csv("Skyserver.csv")
+
+
+# In[26]:
+
+
+pd.read_csv("raw.tsv")
+
+
+# In[28]:
+
+
+pd.read_csv("data.tsv",sep="\t")
+
+
+# In[30]:
+
+
+pd.read_excel(r"C:\Users\user\data.xlsx")
+
+
+# In[33]:
+
+
+pd.read_csv(r"C:\Users\user\Skyserver.csv")
+
+
+# In[34]:
+
+
+pd.read_csv(r"data.tsv")
+
+
+# In[35]:
+
+
+import keyword
+
+
+# In[37]:
+
+
+print(keyword.kwlist)
+
+
+# In[39]:
+
+
+True is True
+
+
+# In[40]:
+
+
+False is False
+
+
+# In[41]:
+
+
+False is True
+
+
+# In[42]:
+
+
+True is False
+
+
+# In[43]:
+
+
+1 is 1
+
+
+# In[44]:
+
+
+False and True is False
+
+
+# In[45]:
+
+
+False and True
+
+
+# In[46]:
+
+
+True and False
+
+
+# In[47]:
+
+
+True and False is True
+
+
+# In[59]:
+
+
+y=8
+t=7
+print(y+t)
+type(y+t)
+
+
+# In[62]:
+
+
+u=9i
+y=8p
+t=u+y
+type(t)
+
+
+# In[ ]:
+
+
+
+
