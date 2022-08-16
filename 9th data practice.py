16a5a5c53268da9f7ca338ea72208e4415d02781
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-17 01:20:36 +0530

    Add files via upload

---
+++ 9th data practice.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+T=98
+R=78
+D=98
+G=74
+E=(T+D)/(R-G)
+print(E)
+type(E)
+
+
+# In[3]:
+
+
+#IF YOU ARE TAGGING BLOCK LETTERS THEN PITHON VERY INTELLIGENT TO RECOGNIZE ITS BLOCK LETTERS ONLY INSTEAD OF SMALL LETTERS.
+
+
+# In[6]:
+
+
+T=78
+R=13
+D=15
+U=(T/R)*D
+print(U)
+type(U)
+
+
+# In[7]:
+
+
+y=78
+r=56
+w=23
+t=y*r+w
+print(t)
+type(t)
+
+
+# In[8]:
+
+
+y=78
+r=56
+w=23
+t=y-r+w
+print(t)
+type(t)
+
+
+# In[9]:
+
+
+t=78
+y=13
+r=t/y
+print(r)
+type(r)
+
+
+# In[10]:
+
+
+#(*,-,+) amount of these sign makes integer
+#(/)these sign makes float
+
+
+# In[13]:
+
+
+t=1.5
+r=7.8
+w=t+r
+print(w)
+type(w)
+
+
+# In[14]:
+
+
+# any fraction no makes float
+
+
+# In[15]:
+
+
+import os
+
+
+# In[18]:
+
+
+os.getcwd()
+
+
+# In[19]:
+
+
+os.getcwd()
+
+
+# In[20]:
+
+
+os.getcwd()
+
+
+# In[21]:
+
+
+os.getcwd()
+
+
+# In[22]:
+
+
+import pandas as pd
+
+
+# In[23]:
+
+
+pd.read_excel(r"C:\Users\user\tools.xlsx")
+
+
+# In[24]:
+
+
+pd.read_excel("tools.xlsx")
+
+
+# In[25]:
+
+
+pd.read_csv(r"C:\Users\user\bikes.csv")
+
+
+# In[5]:
+
+
+pd.read_csv("bikes.csv")
+
+
+# In[31]:
+
+
+pd.read_csv(r"C:\Users\user\Social_Network_Ads.csv")
+
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\tools.csv")
+
+
+# In[8]:
+
+
+pd.read_csv(r"C:\Users\user\rrr.csv")
+
+
+# In[9]:
+
+
+pd.read_csv("rrr.csv")
+
+
+# In[10]:
+
+
+pd.read_csv(r"C:\Users\user\team.tsv.txt")
+
+
+# In[11]:
+
+
+pd.read_csv("team.tsv.txt")
+
+
+# In[12]:
+
+
+pd.read_csv("team.tsv.txt",sep="\t")
+
+
+# In[14]:
+
+
+pd.read_csv(r"C:\Users\user\file.tsv.txt",sep="\t")
+
+
+# In[16]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/dataset1/master/empl.csv")
+
+
+# In[17]:
+
+
+pd.read_csv(r"C:\Users\user\empl.csv")
+
+
+# In[19]:
+
+
+pd.read_csv("empl.csv")
+
+
+# In[21]:
+
+
+True is False
+
+
+# In[22]:
+
+
+True is True
+
+
+# In[23]:
+
+
+False is False
+
+
+# In[24]:
+
+
+False is True
+
+
+# In[25]:
+
+
+1 is 0
+
+
+# In[26]:
+
+
+8 is 8
+
+
+# In[27]:
+
+
+9is 8
+
+
+# In[29]:
+
+
+True or False
+
+
+# In[30]:
+
+
+True and False
+
+
+# In[31]:
+
+
+#and means = sign
+
+
+# In[32]:
+
+
+True is False
+
+
+# In[33]:
+
+
+True and False or True
+
+
+# In[35]:
+
+
+u=5
+c=5i
+r=u+c
+print(r)
+type(r)
+
+
+# In[36]:
+
+
+import keyword
+
+
+# In[37]:
+
+
+print(keyword.kwlist)
+
+
+# In[38]:
+
+
+import keyword
+
+
+# In[39]:
+
+
+print(keyword.kwlist)
+
+
+# In[40]:
+
+
+import keyword
+
+
+# In[41]:
+
+
+print(keyword.kwlist)
+
+
+# In[42]:
+
+
+False,None,True,_peg_parser_, and as assert async await break class continue def del elif else except finally for from global if is in import lambda nonlocal not or pass raise return yield try with while 
+
+
+# In[43]:
+
+
+import keyword
+
+
+# In[44]:
+
+
+print(keyword.kwlist)
+
+
+# In[46]:
+
+
+import os
+
+
+# In[47]:
+
+
+os.getcwd()
+
+
+# In[ ]:
+
+
+import pandas as pd
+
