2c13bbf723e12e0808b8362481d3e34c8c8e2f41
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-19 11:06:35 +0530

    Add files via upload

---
+++ 10th practice data.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+U=987
+I=458
+R=857
+Y=(U+I+R)
+print(Y)
+type(Y)
+
+
+# In[3]:
+
+
+r=99
+t=33
+s=57
+h=19
+y=(r/t)+(s/h)
+print(y)
+type(y)
+
+
+# In[4]:
+
+
+r=99
+t=33
+s=57
+h=19
+y=(r/t)-(s/h)
+print(y)
+type(y)
+
+
+# In[6]:
+
+
+r=99
+t=33
+s=57
+h=19
+y=(r*t)-(s+h)
+print(y)
+type(y)
+
+
+# In[7]:
+
+
+r=99
+t=33
+s=57
+h=19
+y=(r*t)-(s*h)
+print(y)
+type(y)
+
+
+# In[8]:
+
+
+import os
+
+
+# In[9]:
+
+
+os.getcwd()
+
+
+# In[11]:
+
+
+import keyword
+
+
+# In[12]:
+
+
+print(keyword.kwlist)
+
+
+# In[13]:
+
+
+import pandas as pd
+
+
+# In[14]:
+
+
+pd.read_excel(r"C:\Users\user\tools.xlsx")
+
+
+# In[15]:
+
+
+pd.read_excel("tools.xlsx")
+
+
+# In[16]:
+
+
+pd.read_csv(r"C:\Users\user\empl.csv")
+
+
+# In[18]:
+
+
+pd.read_csv("rrr.csv")
+
+
+# In[20]:
+
+
+pd.read_csv(r"C:\Users\user\expense.tsv")
+
+
+# In[21]:
+
+
+pd.read_csv("expense.tsv")
+
+
+# In[23]:
+
+
+pd.read_csv("expense.tsv",sep="\t")
+
+
+# In[26]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/dataset1/master/titanic_train.csv")
+
+
+# In[27]:
+
+
+pd.read_csv("titanic_train.csv")
+
+
+# In[28]:
+
+
+import os
+
+
+# In[29]:
+
+
+os.getcwd()
+
+
+# In[30]:
+
+
+import keyword
+
+
+# In[31]:
+
+
+print(keyword.kwlist)
+
+
+# In[32]:
+
+
+True is True
+
+
+# In[33]:
+
+
+True and False
+
+
+# In[34]:
+
+
+False is False
+
+
+# In[35]:
+
+
+0 is 1
+
+
+# In[36]:
+
+
+1 is 1
+
+
+# In[37]:
+
+
+t=78
+r=95
+y=96
+e=t+r+y
+print(e)
+type(e)
+
+
+# In[38]:
+
+
+r=98
+w=14
+f=45
+c=36
+d=(r/w)+(f-c)
+print(d)
+type(d)
+
+
+# In[39]:
+
+
+pd.read_excel(r"C:\Users\user\tools.xlsx")
+
+
+# In[40]:
+
+
+pd.read_excel("tools.xlsx")
+
+
+# In[42]:
+
+
+pd.read_csv(r"C:\Users\user\empl.csv")
+
+
+# In[43]:
+
+
+pd.read_csv("empl.csv")
+
+
+# In[46]:
+
+
+pd.read_csv("expense.csv")
+
+
+# In[48]:
+
+
+pd.read_csv("expense.tsv")
+
+
+# In[49]:
+
+
+pd.read_csv("expense.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
