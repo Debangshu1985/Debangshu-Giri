f471fb952f3c53bad4f03796f4e4f66545c669e6
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-23 08:12:25 +0530

    Add files via upload

---
+++ 12th data practice.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+firstname='Debangshu'
+lastname='Giri'
+print(firstname,lastname)
+
+
+# In[6]:
+
+
+firstname="Debangshu"
+lastname="giri"
+print(firstname,lastname)
+
+
+# In[7]:
+
+
+a=89
+b=98
+print(a+b)
+
+
+# In[8]:
+
+
+#dsrscientist
+
+
+# In[12]:
+
+
+x=10
+
+
+# In[19]:
+
+
+o="value of x = 10"
+print(o)
+
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.getcwd()
+
+
+# In[3]:
+
+
+import keyword
+
+
+# In[6]:
+
+
+print(keyword.kwlist)
+
+
+# In[7]:
+
+
+print(keyword.kwlist)
+
+
+# In[8]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+pd.read_csv(r"C:\Users\user\Desktop\quotation.csv")
+
+
+# In[13]:
+
+
+F="any excel file can save in comma by this process, file>export>choose the comma>save as>save"
+print(F)
+
+
+# In[14]:
+
+
+pd.read_excel(r"C:\Users\user\Desktop\MICT - ESTIMATE - OVERHAUL.xlsx")
+
+
+# In[15]:
+
+
+pd.read_excel(r"1235.xlsx")
+
+
+# In[16]:
+
+
+pd.read_csv("quotation.csv")
+
+
+# In[17]:
+
+
+pd.read_csv(r"C:\Users\user\file.tsv.txt")
+
+
+# In[18]:
+
+
+pd.read_csv("file.tsv.txt")
+
+
+# In[19]:
+
+
+pd.read_csv("file.tsv.txt",sep="\t")
+
+
+# In[20]:
+
+
+print(keyword.kwlist)
+
+
+# In[21]:
+
+
+False is False
+
+
+# In[22]:
+
+
+True is True
+
+
+# In[23]:
+
+
+False and False
+
+
+# In[25]:
+
+
+True and True
+
+
+# In[28]:
+
+
+False is True
+
+
+# In[29]:
+
+
+True is False
+
+
+# In[30]:
+
+
+True and False is True
+
+
+# In[31]:
+
+
+True and False is False
+
+
+# In[32]:
+
+
+type(print)
+
+
+# In[35]:
+
+
+True or False
+
+
+# In[36]:
+
+
+True + True
+
+
+# In[37]:
+
+
+True - True
+
+
+# In[38]:
+
+
+True + False -True
+
+
+# In[39]:
+
+
+5*True
+
+
+# In[46]:
+
+
+3*True * 140*True * False
+
+
+# In[49]:
+
+
+r=78
+t=13
+g=56
+w=r/t+g
+print(w)
+type(w)
+
+
+# In[50]:
+
+
+r=789
+t=963
+j=999
+s=666
+h=r+s+j-t
+print(h)
+type(h)
+
+
+# In[ ]:
+
+
+
+
