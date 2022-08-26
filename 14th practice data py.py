da59ebbc7453d5b25bcc2c5cf06d7b96bd74f307
Debangshu Giri <110330554+Debangshu1985@users.noreply.github.com>
2022-08-26 07:55:54 +0530

    Add files via upload

---
+++ 14th practice data py.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+y='Debangshu Giri'
+u='I am'
+w='AWM of ASL Motors'
+print(u,y,w)
+
+
+# In[4]:
+
+
+True is True
+
+
+# In[5]:
+
+
+True is False
+
+
+# In[6]:
+
+
+True + False
+
+
+# In[12]:
+
+
+r=(True*118)/(True*2)
+print(r)
+type(r)
+
+
+# In[13]:
+
+
+import os
+
+
+# In[15]:
+
+
+os.getcwd()
+
+
+# In[17]:
+
+
+import pandas as pd
+
+
+# In[18]:
+
+
+import keyword
+
+
+# In[19]:
+
+
+print(keyword.kwlist)
+
+
+# In[20]:
+
+
+True and True
+
+
+# In[21]:
+
+
+True is False
+
+
+# In[22]:
+
+
+False is False
+
+
+# In[23]:
+
+
+False and False
+
+
+# In[24]:
+
+
+y=21*True/(3*True)
+print(y)
+type(y)
+
+
+# In[25]:
+
+
+import os
+
+
+# In[26]:
+
+
+os.getcwd()
+
+
+# In[27]:
+
+
+import keyword
+
+
+# In[28]:
+
+
+print(keyword.kwlist)
+
+
+# In[29]:
+
+
+import pandas as pd
+
+
+# In[39]:
+
+
+pd.read_csv(r"https://raw.githubusercontent.com/dsrscientist/dataset1/master/empl.csv")
+
+
+# In[41]:
+
+
+pd.read_csv(r"C:\Users\user\empl.csv")
+
+
+# In[42]:
+
+
+pd.read_csv("empl.csv")
+
+
+# In[43]:
+
+
+pd.read_excel(r"C:\Users\user\Attendance.xlsx")
+
+
+# In[44]:
+
+
+pd.read_excel("Attendance.xlsx")
+
+
+# In[45]:
+
+
+pd.read_csv(r"D:\data 1\file.tsv.txt")
+
+
+# In[46]:
+
+
+pd.read_csv(r"D:\data 1\file.tsv.txt",sep="\t")
+
+
+# In[48]:
+
+
+o='Debangshu'
+p='Giri'
+k='now'
+l='worked at asl motors'
+d='as awm'
+# as i when print dont need to make it serial wise, if you need as you want.
+print(o,p,k,l,d)
+print(p,o,l,d,k)
+
+
+# In[ ]:
+
+
+
+
